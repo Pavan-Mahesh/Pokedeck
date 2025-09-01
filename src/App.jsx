4ba1bed223c7492708abd3ea0e927d3fe1a64b49
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2025-09-01 22:40:37 +0530

    more-changes
    all this to fit in the phone properly

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     const mainElem = React.useRef(null);
 
     return (
-        <main ref={mainElem} className="w-screen h-dvh flex flex-col bg-slate-100 font-mpm-cursive font-semibold overflow-y-scroll overflow-x-hidden overscroll-none">
+        <main ref={mainElem} className="w-screen h-svh flex flex-col bg-slate-100 font-mpm-cursive font-semibold overflow-y-scroll overflow-x-hidden overscroll-none">
             <PokemonGrid mainElem={mainElem} />
         </main>
     )
