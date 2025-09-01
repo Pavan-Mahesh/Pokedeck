4600983a379feed53bbf861355c8a9e919b4230f
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2025-09-02 00:39:13 +0530

    fixes

--- src/components/EvolutionChain.jsx
+++ src/components/EvolutionChain.jsx
@@ -8,45 +8,41 @@ function EvolutionChain({ setIsShowingEvolutionChain, showEvolutionChain, setSho
     const [isClosing, setIsClosing] = React.useState(false);
 
     return (
-        <div className={`
-            w-full h-svh pt-8 p-4 fixed z-10 inset-0 bg-black/80 overflow-y-auto overflow-x-hidden
-            ${isClosing ? 'animate-mpm-fade-out' : 'animate-mpm-fade-in'}
-        `}>
-            <div
-                className={`w-full min-h-svh flex justify-center`}
-                onAnimationEnd={() => {
-                    if (isClosing) {
-                        setIsClosing(false);
-                        setIsShowingEvolutionChain(false);
-                        setShowEvolutionChain([]);
-                    }
+        <div
+            className={`w-full h-full flex justify-center pt-8 p-4 overflow-y-auto overflow-x-hidden fixed overscroll-none z-10 inset-0 bg-black/80
+                    ${isClosing ? 'animate-mpm-fade-out' : 'animate-mpm-fade-in'}`}
+            onAnimationEnd={() => {
+                if (isClosing) {
+                    setIsClosing(false);
+                    setIsShowingEvolutionChain(false);
+                    setShowEvolutionChain([]);
+                }
+            }}
+        >
+            <div className={`my-auto flex flex-wrap justify-center gap-x-8 gap-y-14`}> {
+                showEvolutionChain.length === 0
+                    ? <Load />
+                    : (
+                        showEvolutionChain.map((path, idx) => {
+                            return (
+                                <div className={`h-max shrink-0 flex flex-col xl:flex-row justify-center gap-6 bg-slate-300/60 py-8 px-6 rounded-xl overflow-y-visible`} key={'mpm-'+idx}>{
+                                    path.map((stage) => {
+                                        const pokemonData = pokemonInfo[stage.name];
+                                        return <Pokemon key={'mpm-'+pokemonData.name} pokemon={pokemonData} />
+                                    })
+                                }</div>
+                            )
+                        })
+                    )
+            } </div>
+            <button
+                className={`w-12 h-12 rounded-full fixed right-6 top-6 bg-black/60 text-white text-xl p-2 cursor-pointer`}
+                onClick={() => {
+                    setIsClosing(true);
                 }}
             >
-                <div className={`my-auto flex flex-wrap justify-center gap-x-8 gap-y-14`}> {
-                    showEvolutionChain.length === 0
-                        ? <Load />
-                        : (
-                            showEvolutionChain.map((path, idx) => {
-                                return (
-                                    <div className={`h-max shrink-0 flex flex-col xl:flex-row justify-center gap-6 bg-slate-300/60 py-8 px-6 rounded-xl overflow-y-visible`} key={'mpm-'+idx}>{
-                                        path.map((stage) => {
-                                            const pokemonData = pokemonInfo[stage.name];
-                                            return <Pokemon key={'mpm-'+pokemonData.name} pokemon={pokemonData} />
-                                        })
-                                    }</div>
-                                )
-                            })
-                        )
-                } </div>
-                <button
-                    className={`w-12 h-12 rounded-full fixed right-6 top-6 bg-black/60 text-white text-xl p-2 cursor-pointer`}
-                    onClick={() => {
-                        setIsClosing(true);
-                    }}
-                >
-                    <img src={closeX} alt={`\u26CC`} />
-                </button>
-            </div>
+                <img src={closeX} alt={`\u26CC`} />
+            </button>
         </div>
     );
 }
