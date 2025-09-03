bc6e6c543783c8129baedff75ae8337185cc7c1f
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2025-09-03 09:00:20 +0530

    ui update

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -71,10 +71,11 @@ function Pokemon({ pokemon }) {
                 >
                     <div className={`w-full flex justify-between items-start`}>
                         <div className={`flex flex-col leading-0 font-semibold`}>
-                            <span className={`text-sm text-gray-400`}>
+                            <span className={`text-base text-gray-400`}>
                                 {(pokemon.id).toString().padStart(4, "0")}
-                                {` [${(pokemon.generation[0]).toUpperCase() + pokemon.generation.substring(1)}]`}
+                                {' ( '}{(pokemon.generation[0]).toUpperCase() + pokemon.generation.substring(1)}{' )'}
                             </span>
+
                             <span className={`text-xl`}>{(pokemon.name[0]).toUpperCase() + pokemon.name.substring(1)}</span>
                         </div>
 
@@ -118,7 +119,11 @@ function Pokemon({ pokemon }) {
                 >
                     <div className={`w-full flex justify-between items-start`}>
                         <div className={`flex flex-col leading-0 font-semibold`}>
-                            <span className={`text-sm text-gray-400`}>{(pokemon.id).toString().padStart(4, "0")}</span>
+                            <span className={`text-base text-gray-400`}>
+                                {(pokemon.id).toString().padStart(4, "0")}
+                                {' ( '}{(pokemon.generation[0]).toUpperCase() + pokemon.generation.substring(1)}{' )'}
+                            </span>
+
                             <span className={`text-xl`}>{(pokemon.name[0]).toUpperCase() + pokemon.name.substring(1)}</span>
                         </div>
 
